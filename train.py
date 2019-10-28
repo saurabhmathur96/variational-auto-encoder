bb7885257ca6bf7426aa3d52a365d39366150d08
Saurabh Mathur <saurabhmathur96@gmail.com>
2019-10-28 17:54:50 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,13 @@
+import sys
 
+import torch
+from torchvision import transforms, datasets
+
+
+train_loader = torch.utils.data.DataLoader(
+  datasets.MNIST('data', train=True, download=True,
+    transform=transforms.Compose([
+      transforms.ToTensor(),
+      transforms.Normalize((0.1307,), (0.3081,))
+  ])),
+batch_size=256, shuffle=True)
